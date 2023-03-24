205ee69c783e28b2626d0a7ac094c4648ad3028c
potatodevs <hoshiku1997@gmail.com>
2023-03-24 23:42:04 +0700

    split-view-into-header-home

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -4,29 +4,32 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class About extends CI_Controller {
     public function index()
     {
-        echo "About Page Index";
-        $this->load->model('Users_model'); 
-        //โหลด Model
-        $this->load->database();
-        //โหลด database
+        // echo "About Page Index";
+        // $this->load->model('Users_model'); 
+        // //โหลด Model
+        // $this->load->database();
+        // //โหลด database
 
-        $result = $this->Users_model->get_lists();
-        //เรียกใช้ฟังกชั่นใน model ตัวนั้น โดยตั้งชื่อเป็น $result = $this -> ชื่อไฟล์model->ชื่อฟังก์ชั่นที่เราโหลด
+        // $result = $this->Users_model->get_lists();
+        // //เรียกใช้ฟังกชั่นใน model ตัวนั้น โดยตั้งชื่อเป็น $result = $this -> ชื่อไฟล์model->ชื่อฟังก์ชั่นที่เราโหลด
 
-        //print_r($result);
-        //ในฟังก์ชั่นนี้เราต้องเชื่อมต่อ database ก่อนถึงจะแสดงผลลัพธิ์
+        // //print_r($result);
+        // //ในฟังก์ชั่นนี้เราต้องเชื่อมต่อ database ก่อนถึงจะแสดงผลลัพธิ์
 
-        echo "<br><Br><br>";
-        //เว้นบรรทัด
-        echo "<pre>";
-        //จัด Format ให้ดูดี
-        print_r($result);
-        echo "<pre>";
-        //จัด Format ให้ดูดี
-        echo $result[0]["username"];
+        // echo "<br><Br><br>";
+        // //เว้นบรรทัด
+        // echo "<pre>";
+        // //จัด Format ให้ดูดี
+        // print_r($result);
+        // echo "<pre>";
+        // //จัด Format ให้ดูดี
+        // echo $result[0]["username"];
 
-        $this->db->close();
-        //เป็นการปิด database
+        // $this->db->close();
+        // //เป็นการปิด database
+
+        $this->load->view('header');
+        $this->load->view('home');
 
     }
 
